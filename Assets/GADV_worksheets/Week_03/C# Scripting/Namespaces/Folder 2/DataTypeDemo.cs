065c2b531610456252126220ba1c8c88abe720b9
NezAFool <chong.tengray@gmail.com>
2025-05-08 11:01:49 +0800

    C# wksheet

--- Assets/GADV_worksheets/Week_03/C# Scripting/Namespaces/Folder 2/DataTypeDemo.cs
+++ Assets/GADV_worksheets/Week_03/C# Scripting/Namespaces/Folder 2/DataTypeDemo.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 namespace ProjectNameSpace2{
-public class DemoScript : MonoBehaviour
+public class DataTypeDemo : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
