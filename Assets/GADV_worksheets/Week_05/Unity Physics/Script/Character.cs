960f0fa9c3a373334f2ceb2d3f1e0aa61099af8e
NezAFool <chong.tengray@gmail.com>
2025-05-29 18:32:12 +0800

    physic

---
+++ Assets/GADV_worksheets/Week_05/Unity Physics/Script/Character.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    CharacterController characterController;
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    private Vector3 moveDirection = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        //Gravity
+        moveDirection.y -= gravity*Time.deltaTime;
+        characterController.Move(moveDirection*Time.deltaTime);
+    }
+}
