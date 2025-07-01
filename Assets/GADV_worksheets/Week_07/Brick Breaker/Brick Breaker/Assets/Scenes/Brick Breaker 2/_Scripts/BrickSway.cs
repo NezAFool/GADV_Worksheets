d917f7e05b6d76a89c5a517f19d5ecbce526d64c
NezAFool <chong.tengray@gmail.com>
2025-07-01 18:49:38 +0800

    wk11

--- Assets/GADV_worksheets/Week_07/Brick Breaker/Brick Breaker/Assets/Scenes/Brick Breaker 2/_Scripts/BrickSway.cs
+++ Assets/GADV_worksheets/Week_07/Brick Breaker/Brick Breaker/Assets/Scenes/Brick Breaker 2/_Scripts/BrickSway.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class BrickSway : MonoBehaviour
 {
     
-    public float range = 1f;
+    public float range = 5f;
     private bool check = true;
     private Vector2 direction = new Vector2 (0.2f, 0);
     // Start is called before the first frame update
@@ -18,26 +18,27 @@ public class BrickSway : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (check)
-        {
-            transform.localPosition = direction;
-            range -= 0.2f;
-            if (range == -1f)
-            {
-                check = false;
-            }
-            Debug.Log($"{range}");
-            
-        }
-        else 
-        {
-            transform.localPosition = -direction;
-            range += 0.2f;
-            if (range == 1f) {
-                check = true;
-            }
-            Debug.Log($"{range}");
-        }
+        //if (check)
+        //{
+        //    transform.localPosition = new Vector2 (1,0);
+        //    range -= 0.5f;
+        //    if (range == -1f)
+        //    {
+        //        check = false;
+        //    }
+        //    Debug.Log($"{range}");
+
+        //}
+        //else 
+        //{
+        //    transform.localPosition = new Vector2 (-1,0);
+        //    range += 0.5f;
+        //    if (range == 1f) {
+        //        check = true;
+        //    }
+        //    Debug.Log($"{range} other way");
+        //}
+        transform.localPosition = new Vector3(MathF.Sin((1/2)*Time.deltaTime*range), 0);
 
     }
 }
--- Assets/GADV_worksheets/Week_07/Brick Breaker/Brick Breaker/Assets/Scenes/Brick Breaker 2/_Scripts/GameReseter.cs
+++ Assets/GADV_worksheets/Week_07/Brick Breaker/Brick Breaker/Assets/Scenes/Brick Breaker 2/_Scripts/GameReseter.cs
@@ -5,12 +5,13 @@ using UnityEngine.SceneManagement;
 namespace Scenes.Brick_Breaker_2._Scripts {
     public class GameReseter : MonoBehaviour  {
         private int scenes = 0;
-        //public void RandomizeScene()
-        //{
-        //    scenes = RandomNumberGenerator(0, 1);
-        //}
+        public void RandomizeScene()
+        {
+            scenes = Random.Range(0, 1);
+        }
         public void OnCollisionEnter2D(Collision2D other) {
             if (!other.gameObject.CompareTag("Ball")) return;
+            RandomizeScene();
             SceneManager.LoadScene(scenes);
         }
     }
